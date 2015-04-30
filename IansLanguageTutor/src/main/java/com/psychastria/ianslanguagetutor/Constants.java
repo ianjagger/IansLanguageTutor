ac157a790b4f22ce18f05dde7c1627b852a9b143
Ian Jagger <ian.jagger@gmail.com>
2015-04-30 23:49:49 +0100

    Adding contants

---
+++ IansLanguageTutor/src/main/java/com/psychastria/ianslanguagetutor/Constants.java
@@ -0,0 +1,8 @@
+package com.psychastria.ianslanguagetutor;
+
+/**
+ * Created by Ian on 30/04/2015.
+ */
+public class Constants {
+    public static String DOWNLOADPATH = "http://localhost/german/Languages.xml";
+}
